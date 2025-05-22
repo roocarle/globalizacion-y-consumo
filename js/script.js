32d7ae6f5562204dd2c5ebd3478eb4f484883c59
Rocio Carle <rcarle@frro.utn.edu.ar>
2025-05-22 19:47:27 -0300

    add buttons, new styles and dynamics

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let clics = 0;
-
 const startBtn = document.getElementById("startBtn");
 const gridCursos = document.getElementById("gridCursos");
 const descripcion = document.getElementById("descripcionCurso");
@@ -7,21 +5,24 @@ const popup = document.getElementById("popup");
 const final = document.getElementById("final");
 const cita = document.getElementById("cita");
 const textoReflexion = document.getElementById("textoReflexion");
+const verMasBtn = document.getElementById("verMasBtn");
 
 startBtn.addEventListener("click", () => {
   gridCursos.classList.remove("hidden");
+  verMasBtn.classList.remove("hidden");
 });
 
 document.querySelectorAll(".card").forEach(card => {
   card.addEventListener("click", () => {
-    clics++;
     descripcion.classList.remove("hidden");
     descripcion.textContent = card.dataset.desc;
-
-    if (clics >= 3) {
-      popup.classList.remove("hidden");
-    }
   });
+});
+
+verMasBtn.addEventListener("click", () => {
+  popup.classList.remove("hidden");
+  descripcion.classList.add("hidden");
+  verMasBtn.classList.add("hidden"); 
 });
 
 document.getElementById("reflexBtn").addEventListener("click", () => {
@@ -39,7 +40,12 @@ document.querySelectorAll(".choice").forEach(btn => {
     } else {
       textoReflexion.innerHTML = `<em>La educación no es solo para saber más. Es para aprender a pensar, a escuchar, a convivir. Con otros, descubrimos lo que solos no podríamos ver. Acompañar y ser acompañado también es formar parte de algo más grande que uno mismo.</em>`;
     }
-
     cita.classList.remove("hidden");
   });
+
+  document.getElementById("volverBtn").addEventListener("click", () => {
+  cita.classList.add("hidden");
+  final.classList.remove("hidden");
+});
+
 });
